d9acafa1d7e4d9240989dc821f27cc28695e3783
DmitryPodelnik <dmitrypodelnik.developer@gmail.com>
2021-04-18 03:51:58 +0300

    Contact Card 2.0

--- React 14.04.21 Contact card/social-network/src/components/ContactCard.js
+++ React 14.04.21 Contact card/social-network/src/components/ContactCard.js
@@ -15,34 +15,34 @@ class ContactCard extends React.Component {
         return (
             <div id="user">
                 <img id="avatar" src={avatar} className="rounded float-start" alt="avatar" width="150px"></img>
-            <form className="row g-3">
-                <div className="col-md-4">
-                    <label className="form-label">First name</label>
-                    <input type="text" class="form-control" id="validationCustom01" value="Mark" required/>
-                </div>
-                <div class="col-md-4">
-                    <label className="form-label">Last name</label>
-                    <input type="text" className="form-control" id="validationCustom02" value="Otto" required/>
-                </div>
-                <div className="col-md-4">
-                    <label className="form-label">Username</label>
-                    <div className="input-group">
-                        <span className="input-group-text">@</span>
-                        <input type="text" aria-describedby="inputGroupPrepend" required/>
+                <form className="row g-3 needs-validation" novalidate>
+                    <div className="col-md-4">
+                        <label className="form-label">First name</label>
+                        <input type="text" class="form-control" id="validationCustom01" required/>
                     </div>
-                </div>
-                <div class="col-md-6">
-                    <label className="form-label">City</label>
-                    <input type="text" className="form-control" id="validationCustom03" required/>
-                </div>
-                <div class="col-md-6">
-                    <label className="form-label">About</label>
-                    <textarea  className="form-control" id="validationCustom03"></textarea>
-                </div>
-                <div className="col-12" id="saveButton">
-                    <button className="btn btn-primary" type="submit">Save</button>
-                </div>
-            </form>
+                    <div class="col-md-4">
+                        <label className="form-label">Last name</label>
+                        <input type="text" className="form-control" id="validationCustom02" required/>
+                    </div>
+                    <div className="col-md-4">
+                        <label className="form-label">Username</label>
+                        <div className="input-group">
+                            <span className="input-group-text">@</span>
+                            <input type="text" aria-describedby="inputGroupPrepend" required/>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <label className="form-label">City</label>
+                        <input type="text" className="form-control" id="validationCustom03" required/>
+                    </div>
+                    <div class="col-md-6" id="userAbout">
+                        <label className="form-label">About</label>
+                        <textarea className="form-control" id="textAbout"></textarea>
+                    </div>
+                    <div className="col-12" id="saveButton">
+                        <button className="btn btn-primary" type="submit">Save</button>
+                    </div>
+                </form>
             </div>
         )
     }
