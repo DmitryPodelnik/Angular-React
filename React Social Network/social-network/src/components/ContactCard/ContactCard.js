4c7118451ed86d245c7b9b09a0e9869a21fc02bb
DmitryPodelnik <dmitrypodelnik.developer@gmail.com>
2021-06-23 02:22:34 +0300

    v 1.0.4

--- React Social Network/social-network/src/components/ContactCard/ContactCard.js
+++ React Social Network/social-network/src/components/ContactCard/ContactCard.js
@@ -17,31 +17,9 @@ class ContactCard extends React.Component {
         }
 
         this.state = {
+
             countFollowers: 0,
-
-            firstName: {
-                readOnly: true,
-            },
-
-            lastName: {
-                readOnly: true,
-            },
-
-            userName: {
-                readOnly: true,
-            },
-
-            city: {
-                readOnly: true,
-            },
-
-            about: {
-                readOnly: true,
-            },
-
-            email: {
-                readOnly: true,
-            }
+            isReading: true,
         };
 
         this.increaseFolowers = this.increaseFolowers.bind(this);
@@ -59,47 +37,14 @@ class ContactCard extends React.Component {
 
     switchEditMode () {  
 
-        //    $("#saveButton").show();
-        //    $("#uploadFile").show();
-
         this.setState(state => ({
-            firstName: {
-                readOnly: !state.firstName.readOnly,
-            },
-            
-            lastName: {
-                readOnly: !state.firstName.readOnly,
-            },
 
-            userName: {
-                readOnly: !state.firstName.readOnly,
-            },
-
-            city: {
-                readOnly: !state.firstName.readOnly,
-            },
-
-            about: {
-                readOnly: !state.firstName.readOnly,
-            },
-
-            email: {
-                readOnly: !state.firstName.readOnly,
-            },
+            isReading: !state.isReading,
         }));
-
-    
-        //    $("#saveButton").hide();
-        //    $("#uploadFile").hide();
-        //    document.getElementById("firstName").readOnly = true;
-        //    document.getElementById("lastName").readOnly = true;
-        //    document.getElementById("city").readOnly = true;
-        //    document.getElementById("userName").readOnly = true;
-        //    document.getElementById("textAbout").readOnly = true;
     }
 
     initializeData () {
-        document.getElementById("firstName").value = this.data.firstName;
+        //document.getElementById("firstName").value = this.data.firstName;
         document.getElementById("lastName").value = this.data.lastName;
         document.getElementById("city").value = this.data.city;
         document.getElementById("userName").value = this.data.username;
@@ -121,10 +66,13 @@ class ContactCard extends React.Component {
                 <div id="user" onLoad={this.initializeData}>
                     <div id="firstLine">
                         <img id="avatar" src={avatar} className="rounded float-start" alt="avatar" width="150px"></img>
-                        <div id="uploadFile">
-                            <label className="form-label" htmlFor="customFile"></label>
-                            <input type="file" className="form-control-sm" id="customFile" />
-                        </div>
+                        {!this.state.isReading
+                            ? <div id="uploadFile" >
+                                <label className="form-label" htmlFor="customFile"></label>
+                                <input type="file" className="form-control-sm" id="customFile" />
+                            </div>
+                            : null
+                        }
                         <div id="follow">
                             <button id="followers" type="button" className="btn btn-danger" onClick={this.increaseFolowers}>
                                 Follow <span id="countFollowers" className="badge bg-secondary">{this.state.countFollowers}</span>
@@ -138,34 +86,37 @@ class ContactCard extends React.Component {
                     <form className="row g-3 needs-validation" noValidate name="userInfo">
                         <div className="col-md-4">
                             <label className="form-label">First name</label>
-                            <input type="text" className="form-control" id="firstName" readOnly={this.state.firstName.readOnly} /> 
+                            <input type="text" className="form-control" id="firstName" readOnly={this.state.isReading} /> 
                         </div>
                         <div className="col-md-4">
                             <label className="form-label">Last name</label>
-                            <input type="text" className="form-control" id="lastName" readOnly={this.state.lastName.readOnly} />
+                            <input type="text" className="form-control" id="lastName" readOnly={this.state.isReading} />
                         </div>
                         <div className="col-md-4">
                             <label className="form-label">Username</label>
                             <div className="input-group">
                                 <span className="input-group-text">@</span>
-                                <input id="userName" type="text" aria-describedby="inputGroupPrepend" readOnly={this.state.userName.readOnly} />
+                                <input id="userName" type="text" aria-describedby="inputGroupPrepend" readOnly={this.state.isReading} />
                             </div>
                         </div>
                         <div class="col-md-4">
                             <label for="inputEmail" class="form-label">Email address</label>
-                            <input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" readOnly={this.state.email.readOnly}/>
+                            <input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" readOnly={this.state.isReading}/>
                         </div>
                         <div className="col-md-6">
                             <label className="form-label">City</label>
-                            <input type="text" className="form-control" id="city" readOnly={this.state.city.readOnly} />
+                            <input type="text" className="form-control" id="city" readOnly={this.state.isReading} />
                         </div>
                         <div className="col-md-6" id="userAbout">
                             <label className="form-label">About</label>
-                            <textarea className="form-control" id="textAbout" readOnly={this.state.about.readOnly}></textarea>
+                            <textarea className="form-control" id="textAbout" readOnly={this.state.isReading}></textarea>
                         </div>
-                        <div className="col-12" id="saveButton">
-                            <button className="btn btn-primary" onClick={this.saveChanges}>Save</button>
-                        </div>
+                        {!this.state.isReading 
+                            ? <div className="col-12" id="saveButton">
+                                <button className="btn btn-primary" onClick={this.saveChanges}>Save</button>
+                            </div>
+                            : null
+                        }
                     </form>
                 </div>
             )
