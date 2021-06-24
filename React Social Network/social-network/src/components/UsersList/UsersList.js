16952304878b56aaca063e106db318aff8437cad
DmitryPodelnik <dmitrypodelnik.developer@gmail.com>
2021-06-24 21:48:41 +0300

    v 1.2.0

--- React Social Network/social-network/src/components/UsersList/UsersList.js
+++ React Social Network/social-network/src/components/UsersList/UsersList.js
@@ -3,44 +3,6 @@ import ContactCard from "../ContactCard/ContactCard"
 
 import {Link} from 'react-router-dom'
 
-//function UserList(props) {
-    // const {data} = props;
-    // const {username} = props;
-    // const {password} = props;
-    // const {form} = props;
-    // let userComponents = null;
-    // let check = false;
-
-    // if (form == "list") {
-    //     userComponents = data.map(item => {
-    //         return <ContactCard key={item.id} userData={item}/>
-    //     });
-    // }
-    // else if (data.length) { 
-    //     userComponents = data.map(item => {
-    //         if (username == item.username && password == item.password) {
-    //             //form.getElementById("mainForm").style.display = "none";
-    //             //return <ContactCard key={item.id} userData={item}/>
-    //             //localStorage.signInLogOut = "Log Out";
-    //             window.location.href = `/profile?user=${item.username}&firstName=${item.firstName}&lastName=${item.lastName}&city=${item.city}&about=${item.about}`;
-    //             check = true;
-                
-    //             return;
-    //         }
-    //     });
-    // }
-
-    // if (!check) {
-            // return <div>
-            //             <Switch>
-            //                 <Route exact path="/users" render={routeProps => (
-            //                     <UserList data={users} form="list"/>
-            //                 )}/>
-            //                 <Route path="/products/:id(\d+)" component={ContactCard} />
-            //             </Switch>
-            //         </div>
-    //}
-//}
 
 class UsersList extends React.Component{
 
@@ -55,14 +17,34 @@ class UsersList extends React.Component{
             <ul className="navbar-nav">
                 {
                     this.data.map(function(item) {
+                        // return  <li key={item.id}>
+                        //             {/* <NavLink to={`/users/${item.id}`} data={item}>{item.username}</NavLink> */}
+                                    // <Link to={{
+                                    //     pathname: `/users/${item.id}`,
+                                    //     state: { data: item }
+                                    // }}>{item.username}</Link>
+                        //         </li>
                         return  <li key={item.id}>
-                                    {/* <NavLink to={`/users/${item.id}`} data={item}>{item.username}</NavLink> */}
-                                    <Link to={{
-                                        pathname: `/users/${item.id}`,
-                                        state: { data: item }
-                                    }}>{item.username}</Link>
+                                    <div className="card mb-3" id="card">
+                                        <div className="row g-0">
+                                            <div className="col-md-4">
+                                                <img src="..." className="img-fluid rounded-start" alt="..." />
+                                            </div>
+                                            <div className="col-md-8">
+                                                <div className="card-body">
+                                                    <Link to={{ pathname: `/users/${item.id}`,
+                                                                state: { data: item }
+                                                                }}>
+                                                        <h5 className="card-title">{item.username}</h5>
+                                                    </Link>
+                                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </li>
-                    })
+                        })
                 }
             </ul>
         </div>;
