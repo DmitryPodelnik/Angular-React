c13bed7c825531890c7fca08497cd7cd28d32624
DmitryPodelnik <dmitrypodelnik.developer@gmail.com>
2021-08-05 13:27:05 +0300

    v 0.0.4

--- Angular Blog/blog/src/app/app.module.ts
+++ Angular Blog/blog/src/app/app.module.ts
@@ -4,11 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArticleComponent } from './article/article.component';
+import { AuthorizationComponent } from './authorization/authorization.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NewArticleComponent } from './new-article/new-article.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NavComponent } from './nav/nav.component';
+import { RoutesComponent } from './routes/routes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArticleComponent
+    ArticleComponent,
+    AuthorizationComponent,
+    ArticleListComponent,
+    NotFoundComponent,
+    NewArticleComponent,
+    ProfileComponent,
+    NavComponent,
+    RoutesComponent
   ],
   imports: [
     BrowserModule,
