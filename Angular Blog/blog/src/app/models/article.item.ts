0a83d03862c8531786e76d31c7172158db372e1a
DmitryPodelnik <dmitrypodelnik.developer@gmail.com>
2021-08-05 12:48:36 +0300

    v 0.0.2

---
+++ Angular Blog/blog/src/app/models/article.item.ts
@@ -0,0 +1,5 @@
+export class ArticleItem {
+  id: number | undefined;
+  name: string | undefined;
+  isCompleted: boolean | undefined;
+}
---
+++ Angular Blog/blog/src/app/models/dist/article.item.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+exports.ArticleItem = void 0;
+var ArticleItem = /** @class */ (function () {
+    function ArticleItem() {
+    }
+    return ArticleItem;
+}());
+exports.ArticleItem = ArticleItem;
