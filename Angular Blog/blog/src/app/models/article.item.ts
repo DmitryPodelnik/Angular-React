0a79f30e919705fc60fcc061a2d5217034b36e86
DmitryPodelnik <dmitrypodelnik.developer@gmail.com>
2021-08-06 15:08:05 +0300

    v 0.2.2

--- Angular Blog/blog/src/app/models/article.item.ts
+++ Angular Blog/blog/src/app/models/article.item.ts
@@ -2,16 +2,16 @@ export class ArticleItem {
   id: number | undefined;
   title: string;
   content: string;
-  author: string;
-  postDate: string;
-  commentsCount: number;
+  username: string;
+  date: string;
+  comments: any;
+  tags: any;
   file: any | undefined;
 
   constructor () {
     this.content = "";
     this.title = "";
-    this.author = "";
-    this.postDate = "";
-    this.commentsCount = 0;
+    this.username = "";
+    this.date = "";
   }
 }
