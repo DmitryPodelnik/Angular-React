7e5708e82e20ac3a6e03a9047d815898ed37b715
DmitryPodelnik <dmitrypodelnik.developer@gmail.com>
2021-08-08 15:27:21 +0300

    v 0.4.2

--- Angular Blog/blog/src/app/article/article.component.ts
+++ Angular Blog/blog/src/app/article/article.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 
+import { ArticleItem } from '../models/article.item';
+
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
@@ -10,6 +12,7 @@ import { switchMap } from 'rxjs/operators';
 export class ArticleComponent implements OnInit {
 
   id: number | undefined;
+  article: ArticleItem | undefined;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -18,6 +21,18 @@ export class ArticleComponent implements OnInit {
         switchMap(params => params.getAll('id'))
     )
     .subscribe(data => this.id = +data);
+
+    fetch(`https://localhost:44341/api/articles/${this.id}`)
+        .then(res => res.json())
+        .then(
+            data => {
+              console.log('success fetch id article');
+              this.article = data;
+            },
+            error => {
+              console.log(error);
+            }
+        )
   }
 
 }
