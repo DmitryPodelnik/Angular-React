aa6514fe2fc265a0e3bc0c32cc7d8dac01af2a59
DmitryPodelnik <dmitrypodelnik.developer@gmail.com>
2021-08-12 14:50:32 +0300

    v 1.0.0

--- Angular Blog/blog/src/app/new-article/new-article.component.ts
+++ Angular Blog/blog/src/app/new-article/new-article.component.ts
@@ -26,16 +26,54 @@ export class NewArticleComponent implements OnInit {
       tags: this.tags,
     };
 
-    fetch(`https://localhost:44341/api/articles/add?title=${this.title}&content=${this.content}&tags=${this.tags}`)
+    $event.preventDefault();
+
+        if (article.title.length < 1 && article.title.length > 32) {
+            alert("Enter a correct length of title!");
+            return;
+        }
+        else if (article.title.length > 150) {
+            alert("Enter a correct length of tags!");
+            return;
+        }
+        else if (article.content.length > 1000) {
+            alert("Enter a correct length of content!");
+            return;
+        } else {
+
+        let temp = (<HTMLInputElement>document.getElementById("avatar"));
+
+        let fData = new FormData();
+        if (temp != null && temp.files != null) {
+            fData.append("avatar", temp.files[0]); // добавляем файл в объект FormData()
+        }
+
+        fetch(`https://localhost:44341/api/articles/add?title=${this.title}&content=${this.content}&tags=${this.tags}`, {
+            method: "GET",
+            body: fData
+        })
+        .then(res => res.json())
         .then(
             data => {
-              console.log('success');
-              this.router.navigate(['/articles']);
+                alert("Article has been successfully added!");
+                this.router.navigate(['/articles']);
             },
             error => {
-              console.log(error);
-            }
-        )
+                alert(error);
+                console.log(error);
+            })
+        }
+
+    // fetch(`https://localhost:44341/api/articles/add?title=${this.title}&content=${this.content}&tags=${this.tags}`)
+    //     .then(
+    //         data => {
+    //           console.log('success');
+    //           this.router.navigate(['/articles']);
+    //         },
+    //         error => {
+    //           console.log(error);
+    //         }
+    //     )
 
   }
 }
